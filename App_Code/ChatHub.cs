c20c74afd81dcd80bf69658c77bb85763f73ed1a
Mike Brind <mike@mikebrind.com>
2012-12-27 20:18:03 +0000

    Code Commit

---
+++ App_Code/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+
+public class ChatHub : Hub
+{
+    public void Send(string name, string message)
+    {
+        Clients.All.broadcastMessage(name, message);
+    }
+
+    public void IsTyping(string name){
+        Clients.All.sayWhoIsTyping(name);
+    }
+}
---
+++ App_Code/ProductHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using WebMatrix.Data;
+
+public class ProductHub : Hub
+{
+    public void BuyProduct(int productId) {
+        using (var db = Database.Open("Northwind")) {
+            var unitsInStock = db.QueryValue("SELECT UnitsInStock FROM Products WHERE ProductId = @0", productId);
+            if (unitsInStock > 0) {
+                db.Execute("UPDATE Products Set UnitsInStock = UnitsInStock - 1 WHERE ProductId = @0", productId);
+                unitsInStock -= 1;
+            }
+            Clients.All.updateAvailableStock(unitsInStock, productId);
+        }
+    }
+
+    public void ReorderProduct(int quantity, int productId){
+        using (var db = Database.Open("Northwind")) {
+            db.Execute("UPDATE Products Set UnitsInStock = UnitsInStock + @0 WHERE ProductId = @1", quantity, productId);
+            var unitsInStock = db.QueryValue("SELECT UnitsInStock FROM Products WHERE ProductId = @0", productId);
+            Clients.All.updateAvailableStock(unitsInStock, productId);
+        }
+    }
+}
